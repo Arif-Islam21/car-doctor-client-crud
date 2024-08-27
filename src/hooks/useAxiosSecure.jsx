549a86a469f3f8b7dda091b27909d0ef0fa3a7d4
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-27 16:49:26 +0600

    done

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+
+const axiosSecure = axios.create({
+  baseURL: "http://localhost:5000",
+  withCredentials: true,
+});
+
+const useAxiosSecure = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axiosSecure.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (err) => {
+        console.log("error trackted in the interceptor", err.response);
+        if (err.response.status === 401 || err.response.status === 403) {
+          console.log("logout the user");
+          logOut()
+            .then(() => {
+              navigate("/login");
+            })
+            .catch((err) => console.log(err));
+        }
+      }
+    );
+  }, []);
+
+  return axiosSecure;
+};
+
+export default useAxiosSecure;
